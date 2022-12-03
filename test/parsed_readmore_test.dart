88f26cb4bb2a5871ff0b87df4be249fc0920182c
Samuel Cousin <stomcee@gmail.com>
2022-12-04 00:14:45 +0100

    Update parsed_readmore_test.dart

--- test/parsed_readmore_test.dart
+++ test/parsed_readmore_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:parsed_readmore/parsed_readmore.dart';
+import 'package:advparsed_readmore/parsed_readmore.dart';
 
 void main() {
   test('adds one to input values', () {});
